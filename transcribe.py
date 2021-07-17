4dc3b9dedc20b13ebedb26b4abf13e11c2baac11
waadalsaedii <86182384+waadalsaedii@users.noreply.github.com>
2021-07-17 12:24:41 +0300

    Add files via upload

---
+++ transcribe.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python
+#
+# Copyright 2016 IBM
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import argparse
+import base64
+import configparser
+import json
+import threading
+import time
+
+import pyaudio
+import websocket
+from websocket._abnf import ABNF
+from playsound import playsound
+
+from ibm_watson.websocket import SynthesizeCallback
+from ibm_watson import TextToSpeechV1
+from ibm_watson import AssistantV2
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+ #Setup TTS service 
+authenticator = IAMAuthenticator('cyeWo8K-_8AMkBCraFIXOVwaa4SKVHLmcH9NQEmEsvEU') 
+tts = TextToSpeechV1(authenticator=authenticator)
+tts.set_service_url('https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/0ae887a4-f2fe-4240-87c7-16bb3c31d086')
+
+#Setup Assistant service
+authenticator = IAMAuthenticator('87p1aPsk7hswcXOd43Z0dFG6TzZ89iN3oC-TwqByGE5I')
+assistant = AssistantV2(version='2020-04-01', authenticator = authenticator)
+assistant.set_service_url('https://api.us-south.assistant.watson.cloud.ibm.com/instances/c862d8a6-86e8-48ce-81fd-20175018abad')
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+# Even if your default input is multi channel (like a webcam mic),
+# it's really important to only record 1 channel, as the STT service
+# does not do anything useful with stereo. You get a lot of "hmmm"
+# back.
+CHANNELS = 1
+# Rate is important, nothing works without it. This is a pretty
+# standard default. If you have an audio device that requires
+# something different, change this.
+RATE = 44100
+RECORD_SECONDS = 5
+FINALS = []
+LAST = None
+
+REGION_MAP = {
+    'us-east': 'gateway-wdc.watsonplatform.net',
+    'us-south': 'stream.watsonplatform.net',
+    'eu-gb': 'stream.watsonplatform.net',
+    'eu-de': 'stream-fra.watsonplatform.net',
+    'au-syd': 'gateway-syd.watsonplatform.net',
+    'jp-tok': 'gateway-syd.watsonplatform.net',
+}
+
+
+def read_audio(ws, timeout):
+    """Read audio and sent it to the websocket port.
+    This uses pyaudio to read from a device in chunks and send these
+    over the websocket wire.
+    """
+    global RATE
+    p = pyaudio.PyAudio()
+    # NOTE(sdague): if you don't seem to be getting anything off of
+    # this you might need to specify:
+    #
+    #    input_device_index=N,
+    #
+    # Where N is an int. You'll need to do a dump of your input
+    # devices to figure out which one you want.
+    RATE = int(p.get_default_input_device_info()['defaultSampleRate'])
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    print("* recording")
+    rec = timeout or RECORD_SECONDS
+
+    for i in range(0, int(RATE / CHUNK * rec)):
+        data = stream.read(CHUNK)
+        # print("Sending packet... %d" % i)
+        # NOTE(sdague): we're sending raw binary in the stream, we
+        # need to indicate that otherwise the stream service
+        # interprets this as text control messages.
+        ws.send(data, ABNF.OPCODE_BINARY)
+
+    # Disconnect the audio stream
+    stream.stop_stream()
+    stream.close()
+    print("* done recording")
+
+    # In order to get a final response from STT we send a stop, this
+    # will force a final=True return message.
+    data = {"action": "stop"}
+    ws.send(json.dumps(data).encode('utf8'))
+    # ... which we need to wait for before we shutdown the websocket
+    time.sleep(1)
+    ws.close()
+
+    # ... and kill the audio device
+    p.terminate()
+
+
+def on_message(self, msg):
+    """Print whatever messages come in.
+    While we are processing any non trivial stream of speech Watson
+    will start chunking results into bits of transcripts that it
+    considers "final", and start on a new stretch. It's not always
+    clear why it does this. However, it means that as we are
+    processing text, any time we see a final chunk, we need to save it
+    off for later.
+    """
+    global LAST
+    data = json.loads(msg)
+    if "results" in data:
+        if data["results"][0]["final"]:
+            FINALS.append(data)
+            LAST = None
+        else:
+            LAST = data
+        # This prints out the current fragment that we are working on
+        output_text = data['results'][0]['alternatives'][0]['transcript']
+        print(output_text)
+        #STT
+        with open ('output.txt','w') as text:
+          text.writelines(output_text)
+          text.close()
+        print (json.dumps(response, indent=2))
+        
+def on_error(self, error):
+    """Print any errors."""
+    print(error)
+
+
+def on_close(ws):
+    """Upon close, print the complete and final transcript."""
+    global LAST
+    if LAST:
+        FINALS.append(LAST)
+    transcript = "".join([x['results'][0]['alternatives'][0]['transcript']
+                          for x in FINALS])
+    print(transcript)
+
+
+def on_open(ws):
+    """Triggered as soon a we have an active connection."""
+    args = ws.args
+    data = {
+        "action": "start",
+        # this means we get to send it straight raw sampling
+        "content-type": "audio/l16;rate=%d" % RATE,
+        "continuous": True,
+        "interim_results": True,
+        # "inactivity_timeout": 5, # in order to use this effectively
+        # you need other tests to handle what happens if the socket is
+        # closed by the server.
+        "word_confidence": True,
+        "timestamps": True,
+        "max_alternatives": 3
+    }
+
+    # Send the initial control message which sets expectations for the
+    # binary stream that follows:
+    ws.send(json.dumps(data).encode('utf8'))
+    # Spin off a dedicated thread where we are going to read and
+    # stream out audio.
+    threading.Thread(target=read_audio,
+                     args=(ws, args.timeout)).start()
+
+def get_url():
+    config = configparser.RawConfigParser()
+    config.read('speech.cfg')
+   
+    region = config.get('auth', 'region')
+    host = REGION_MAP[region]
+    return ("wss://{}/speech-to-text/api/v1/recognize"
+           "?model=en-AU_BroadbandModel").format(host)
+
+def get_auth():
+    config = configparser.RawConfigParser()
+    config.read('speech.cfg')
+    apikey = config.get('auth', 'apikey')
+    return ("apikey", apikey)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Transcribe Watson text in real time')
+    parser.add_argument('-t', '--timeout', type=int, default=5)
+    # parser.add_argument('-d', '--device')
+    # parser.add_argument('-v', '--verbose', action='store_true')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    # Connect to websocket interfaces
+    headers = {}
+    userpass = ":".join(get_auth())
+    headers["Authorization"] = "Basic " + base64.b64encode(
+        userpass.encode()).decode()
+    url = get_url()
+
+           
+    ws = websocket.WebSocketApp(url,
+                                header=headers,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+    ws.on_open = on_open
+    ws.args = parse_args()
+    
+    ws.run_forever()
+    
+   
+    with open('test.txt', 'r') as f:
+     tts_Speech = f.readlines()
+     tts_Speech = [line.replace('\n', '') for line in tts_Speech]
+     tts_Speech = ''.join(str(line) for line in tts_Speech)
+     
+    #Respons Assistant
+    response = assistant.message_stateless(
+     assistant_id='9dd180d0-75e4-451b-b399-7b8bd0b1aea1', 
+     input={ 'message_type': 'text',  'text': tts_Speech }
+    ).get_result()
+    text   = json.dumps(response, indent=2)
+    write  = json.loads(text)
+    speech = json.dumps(write["output"]["generic"][0].get("text"))  
+    with open('voice.mp3', 'wb') as a:
+      res = tts.synthesize(speech, accept='audio/mp3', voice='en-US_AllisonV3Voice').get_result()
+      a.write(res.content)
+      a.close() 
+    print(speech) 
+    playsound('voice.mp3') 
+  
+if __name__ == "__main__":
+    main()
